626480159f672c30f71e93651df722f488262ff0
104198 <104198@U-55178.domena.uksw.edu.pl>
2019-10-26 12:27:42 +0200

    Po długiej i uciążliwej pracy - udałło się! Oto 'Hello World!'.

---
+++ proj/src/proj/HelloWorld.java
@@ -0,0 +1,14 @@
+package proj;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		String hello = "Hello world!";
+		
+		System.out.println("Napis " + hello + " ma " + hello.length() + " znakow.");
+		
+	}
+}
